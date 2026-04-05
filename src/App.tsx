bcfd77213b01f5c2ba8630d20fd8e1f0e9d180e7
“miwakota” <m0ko305@icloud.com>
2026-04-05 17:30:07 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-base-200">
+      <div className="hero min-h-screen">
+        <div className="hero-content text-center">
+          <div>
+            <h1 className="text-5xl font-bold">My Portfolio</h1>
+            <p className="py-6">
+              React + TypeScript + Tailwind + daisyUI
+            </p>
+            <button className="btn btn-primary">Worksを見る</button>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default App
